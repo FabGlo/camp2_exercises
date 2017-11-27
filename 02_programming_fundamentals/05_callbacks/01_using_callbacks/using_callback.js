15536e08de38e1732b3685fd2243c62baaae9902
Fabien Glory <fabien.glory@decathlon.com>
2017-11-27 13:13:15 +0100

    callback 01/02

--- 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
+++ 02_programming_fundamentals/05_callbacks/01_using_callbacks/using_callback.js
@@ -1,7 +1,10 @@
 const thisIsThePlayer = (callback) => {
   const player = { name: "Spartacus", life: 100, strength: 100, agility: 100 };
   callback(player);
-}
+};
+
+const sayHello = (player) => console.log("Hello, " + player.name);
+
 
 // Write a function sayHello that greets: "Hello, Spartacus". (Where "Spartacus" is the player name)
 
