cc60ff9ca62e99ba754cca6e8684d6e1449f3e37
Fabien Glory <fabien.glory@decathlon.com>
2017-11-27 11:48:40 +0100

    algo2

--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/01_filter_with_string.js
@@ -1,8 +1,30 @@
 // filter takes an array of integer and a string with either odd or even
 // such as filter([1, 2, 3, 4, 5], 'even') returns [2, 4]
 function filter(array, str) {
-  // Your code here
+  const arrayFilter = [];
+  if (str === "even"){
+    for(let i=0 ; i < array.length ; i++){
+      if (array[i] % 2 === 0){
+        arrayFilter.push(array[i]);
+      }
+    }
+  }
+  else if (str === "odd"){
+    for(let j=0 ; j < array.length ; j++){
+      if (array[j] % 2 === 1){
+        arrayFilter.push(array[j]);
+      }
+    }
+  }
+  else if (str !== "odd" && str!== "even"){
+    for(let k=0 ; k < array.length ; k++){
+      arrayFilter.push(array[k]);
+    }
+  }
+  return arrayFilter;
 }
+
+filter([1, 2, 3, 4, 5], "even");
 
 // do not remove this line, it is for tests
 module.exports = filter;
--- 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
+++ 02_programming_fundamentals/04_algorithms_day_2/04_filter/02_filter_with_function.js
@@ -1,8 +1,39 @@
 // filter takes an array of integer and a function of filtering
 // such as filter([1, 2, 3, 4, 5], pickEvenNumbers) returns [2, 4]
 function filter(array, fn) {
-  // Your code here
+  if (fn === "pickEvenNumbers"){
+    return pickEvenNumbers(array);
+  }
+  else if
+  (fn ==="pickOddNumbers"){
+    return pickOddNumbers(array);
+  }
+  else{
+    return array;
+  }
 }
+
+function pickEvenNumbers(number) {
+  const arrayE = [];
+  for (let i =0 ; i < number.length; i++){
+    if (number[i] % 2 ===0){
+      arrayE.push(number[i]);
+    }
+  }
+  return arrayE;
+}
+
+function pickOddNumbers(number) {
+  const arrayO = [];
+  for (let j =0 ; j < number.length; j++){
+    if (number[j] % 2 ===1){
+      arrayO.push(number[j]);
+    }
+  }
+  return arrayO;
+}
+
+filter([1, 2, 3, 4, 5],pickEvenNumbers);
 
 // do not remove this line, it is for tests
 module.exports = filter;
