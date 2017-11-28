83fa6ad8cc97dace12f01ed3017b13a0d38b1da7
Fabien Glory <fabien.glory@decathlon.com>
2017-11-28 17:52:36 +0100

    sort an array

--- 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
+++ 02_programming_fundamentals/05_callbacks/06_sort_an_array/sort_an_array.js
@@ -7,9 +7,27 @@
 // Note: You should not use Array.sort()
 
 function sort(unsortedArray) {
-  // Your code here
+  let newArray = [];
+  for(let i=0 ; i<unsortedArray.length ; i++){
+    if (newArray.length === 0){
+      newArray.push(unsortedArray[i]);
+    }else{
+      newArray.splice(sorted(unsortedArray[i],newArray) , 0 , unsortedArray[i]);
+    }
+  }
+  return newArray;
 }
 
+function sorted(number,newArray){
+  for(let i=0;i<newArray.length;i++){
+    if (number < newArray[i]){
+      return i;
+    }
+  }
+  return newArray.length;
+}
+
+console.log(sort([24, 7, 9, 72, -8]));
 // Do not remove last lines, it is for tests
 // eslint-disable-next-line
 module.exports = sort;
