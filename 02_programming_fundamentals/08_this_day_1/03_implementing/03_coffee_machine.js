3e7aea48a8cc668e9310c8d4e3a81c6528e5970b
Fabien Glory <fabien.glory@decathlon.com>
2017-11-30 12:22:19 +0100

    Exo 3 OK

--- 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
+++ 02_programming_fundamentals/08_this_day_1/03_implementing/03_coffee_machine.js
@@ -1,6 +1,25 @@
-// See Sparta courses for the exercise summary
+const machine = {
+  litersOfCoffee : 0,
+  fillWithLitersOfCoffee: function(liter){
+    this.litersOfCoffee += liter;
+    return this;
+  },
+  expresso: function(){
+    if (this.litersOfCoffee >= 0.08){
+      this.litersOfCoffee -= 0.08;
+      return true;
+      }
+    return false;
+  },
+  longCoffee: function(){
+    if (this.litersOfCoffee >= 0.15) {
+      this.litersOfCoffee -= 0.15;
+      return true;
+      }
+    return false;
+  }
+};
 
-// Coffee Machine usage. Insert your code above this comment
 
 machine.fillWithLitersOfCoffee(10);
 console.log(machine.expresso()) // => true
@@ -10,5 +29,8 @@ console.log(machine.litersOfCoffee) // => 9.77
 // some more people ordering coffee here
 console.log(machine.litersOfCoffee) // => 0.02
 console.log(machine.expresso()) // => false
+
+
+
 
 module.exports = machine;
