6ce0eba82eff5077a78ffb950a2e2cdc549fe8da
Fabien Glory <fabien.glory@decathlon.com>
2017-12-05 14:33:09 +0100

    EXO 1 OK

--- 03_backend_developement/02_node_&_internet_fundamentals/01_request_the_world/talk_with_postman_echo.js
+++ 03_backend_developement/02_node_&_internet_fundamentals/01_request_the_world/talk_with_postman_echo.js
@@ -1,7 +1,68 @@
-function simpleGet(callback){
+const request = require("request");
 
+function simpleGet(func){
+  request(
+    {
+      url: "https://postman-echo.com/get",
+      method: "GET"
+    },
+    function (error, response, result) {
+      func(result);
+    }
+  );
 }
 
-function callback(result){
-
+function func(result){
+  return result;
 }
+
+simpleGet(func);
+
+
+
+function simpleGetWithParams(func2){
+  request(
+    {
+      url: "https://postman-echo.com/get?foo=bar&program=camp2&people=Frieda&people=Francis",
+      method: "GET"
+    },
+    function (error, response, result) {
+      const resultJSON = JSON.parse(result);
+      return func2(resultJSON.args);
+    }
+  );
+}
+
+function func2(resultParse){
+  return resultParse;
+}
+
+simpleGetWithParams(func2);
+
+
+const today = new Date().toISOString().substring(0, 10);
+
+function validateTimestamp(func3){
+  request(
+    {
+      url: `https://postman-echo.com/time/valid?timestamp=${today}`,
+      method: "GET"
+    },
+    function(error, response, result){
+      return func3(result);
+    }
+  );
+}
+
+function func3(result){
+  return result;
+}
+
+validateTimestamp(func3);
+
+
+module.exports={
+  simpleGet : simpleGet,
+  simpleGetWithParams : simpleGetWithParams,
+  validateTimestamp : validateTimestamp
+};
