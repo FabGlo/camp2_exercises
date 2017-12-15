30c7040c43ed906682f71c0eb3c592bc248187f4
FABIEN GLORY <fabien.glory@decathlon.com>
2017-12-15 10:19:16 +0100

    commerce api ok

--- 03_backend_developement/08_09_commerce_API_project/api_call.js
+++ 03_backend_developement/08_09_commerce_API_project/api_call.js
@@ -2,23 +2,101 @@ const request = require("request");
 const { Pool } = require("pg");
 const pool = new Pool();
 
+function renderproductsbycategories(ProductsByCategoriesJSON,identifier){
+  const parsedResult = JSON.parse(ProductsByCategoriesJSON);
+  pool.connect(
+    (error, client, release) =>{
+      if (error){
+        release();
+        return console.error("Error acquiring client", error.stack);
+      }
+      else
+      {
+        parsedResult.map((newResult) => {
+          return client.query(
+            "INSERT INTO categories_products (id_categories,id_products) VALUES ($1,$2);",
+            [identifier,newResult.id],
+            (error, result)=>{
+              if (error){
+                return console.error("Error acquiring client", error.stack);
+              }
+              return result;
+            }
+          );
+        }
+        );
+        release();
+      }
+    }
+  );
+}
 
-function apicall (parameter,callback) {
+
+function apicallproductsbycategories(identifier,renderproductsbycategoriesCallback){
+  request(
+    {
+      url: `https://decath-product-api.herokuapp.com/categories/${identifier}/products`,
+      method: "GET",
+    },
+    function(error, response, ProductsByCategoriesJSON) {
+      if (error) {
+        console.warn("error:", error);
+      }
+      else {
+        renderproductsbycategoriesCallback(ProductsByCategoriesJSON,identifier);
+      }
+    }
+  );
+}
+
+function renderIdentifier(resultJSON, apicallproductsbycategoriesCallback){
+  for (let i=1; i<200;i++){
+    const identifier = JSON.parse(resultJSON)[i].id;
+    apicallproductsbycategoriesCallback(identifier, renderproductsbycategories);
+  }
+  setTimeout(()=>{
+    for (let j=200; j<400;j++){
+      const identifier = JSON.parse(resultJSON)[j].id;
+      apicallproductsbycategoriesCallback(identifier, renderproductsbycategories);
+    }},100000);
+  setTimeout(()=>{
+    for (let k=400; k<600;k++){
+      const identifier = JSON.parse(resultJSON)[k].id;
+      apicallproductsbycategoriesCallback(identifier, renderproductsbycategories);
+    }},100000);
+  setTimeout(()=>{
+    for (let l=600; l<800;l++){
+      const identifier = JSON.parse(resultJSON)[l].id;
+      apicallproductsbycategoriesCallback(identifier, renderproductsbycategories);
+    }},100000);
+  setTimeout(()=>{
+    for (let m=800; m<1002;m++){
+      const identifier = JSON.parse(resultJSON)[m].id;
+      apicallproductsbycategoriesCallback(identifier, renderproductsbycategories);
+    }},100000);
+}
+
+
+function apicallparameter (parameter, renderIdentifierCallback) {
   request(
     {
       url: `https://decath-product-api.herokuapp.com/${parameter}`,
       method: "GET"
     },
-    function(error, response, result) {
+    function(error, response, resultJSON) {
       if (error){
         console.warn("error:", error);
       }
       else {
-        callback(result);
+        renderIdentifierCallback(resultJSON, apicallproductsbycategories);
       }
     }
   );
 }
+
+apicallparameter("categories", renderIdentifier);
+
+
 
 function renderBrands(result){
   const parsedResult = JSON.parse(result);
@@ -108,8 +186,6 @@ function renderCategories(result){
 }
 
 
-
-
-apicall("brands",renderBrands);
-apicall("products",renderProducts);
-apicall("categories",renderCategories);
+// apicallparameter("brands",renderBrands);
+// apicallparameter("products",renderProducts);
+// apicallparameter("categories",renderCategories);
--- 03_backend_developement/08_09_commerce_API_project/server.js
+++ 03_backend_developement/08_09_commerce_API_project/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const PG = require("pg");
 
 const port = process.env.PORT || 3000;
 
@@ -7,9 +8,71 @@ app.get("/", function (request, result) {
   result.send("Hello World!");
 });
 
-app.get("/parameters", function(request, result) {
-  result.json(request.query);
-});
+function route(parameter){
+  app.get(
+    `/${parameter}`,
+    function(request, result) {
+      const client = new PG.Client();
+      client.connect();
+      client.query(
+        `SELECT * FROM ${parameter};`,
+        function(error, resultQuery) {
+          if (error) {
+            result.send(error);
+          } else {
+            result.json(resultQuery.rows);
+          }
+          client.end();
+        }
+      );
+    }
+  );
+}
+
+function routeid(parameter){
+  app.get(
+    `/${parameter}/:id`,
+    function(request, result) {
+      const client = new PG.Client();
+      client.connect();
+      client.query(
+        `SELECT * FROM ${parameter} WHERE id = $1;`,
+        [request.params.id],
+        function(error, resultQuery) {
+          if (error) {
+            result.send(error);
+          } else {
+            result.json(resultQuery.rows);
+          }
+          client.end();
+        }
+      );
+    }
+  );
+}
+
+app.get(
+  "/categories/:id/products",
+  function(request, result) {
+    const client = new PG.Client();
+    client.connect();
+    client.query(
+      "SELECT products.* FROM products INNER JOIN categories_products on (products.id=categories_products.id_products) WHERE categories_products.id_categories = $1;",
+      [request.params.id],
+      function(error, resultQuery) {
+        if (error) {
+          result.send(error);
+        } else {
+          result.json(resultQuery.rows);
+        }
+        client.end();
+      }
+    );
+  }
+);
+
+
+
 
 app.listen(port, function () {
   console.log("Server listening on port:" + port);
